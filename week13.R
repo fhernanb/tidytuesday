c2b484f2bf716583a6cdf7f18a5fbdb26c9c9817
fhernanb <fhernanb@unal.edu.co>
2018-06-29 19:09:57 -0500

    tabla del artículo

--- week13.R
+++ week13.R
@@ -2,3 +2,14 @@
 dt <- read_csv("week13_alcohol_global.txt")
 dt
 dim(dt)
+
+# Changing names
+colnames(dt) <- c('country', 'beer', 'spirit', 'wine', 'total')
+
+# Packges
+require(dplyr)
+
+# Who drinks the most beer, spirits and wine?
+dt %>% arrange(desc(beer))
+dt %>% arrange(desc(spirit))
+dt %>%  arrange(desc(wine))
